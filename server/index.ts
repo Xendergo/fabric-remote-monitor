4c1ab5ce34b0ea2ca39f17fcc0b5d789fc79226b
Xendergo <h.rovnyak@gmail.com>
2021-06-26 11:19:49 -0400

    get SSL actually working & also switch to forge

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import express from "express"
 import { MinecraftInterface } from "./mc-communication/minecraft-interface"
 import ws from "ws"
-import { execSync } from "child_process"
 import { location } from "./database/Database"
 import fs from "fs"
 import path from "path"
 import https from "https"
+import { pki } from "node-forge"
 
 if (!fs.existsSync(location)) {
     fs.mkdirSync(location, {
@@ -17,9 +17,72 @@ const keyPath = path.join(location, "https.key")
 const certPath = path.join(location, "https.cert")
 if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
     console.log("Generating self signed SSL certificate...")
-    fs.writeFileSync(keyPath, "")
-    fs.writeFileSync(certPath, "")
-    execSync(`openssl req -nodes -new -x509 -keyout "${keyPath}" -out "${certPath}" -passout pass:password -passin pass:password -subj "/C=US/ST=./L=./O=fabric-remote-monitor/OU=fabric-remote-monitor/CN=fabric-remote-monitor"`)
+    const keys = pki.rsa.generateKeyPair(2048)
+    const cert = pki.createCertificate()
+
+    cert.publicKey = keys.publicKey
+    cert.serialNumber = "01"
+    cert.validity.notBefore = new Date()
+    cert.validity.notAfter = new Date()
+    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear()+1)
+
+    const attrs = [{
+        name: 'commonName',
+        value: 'fabric-remote-monitor'
+      }, {
+        name: 'countryName',
+        value: 'US'
+      }, {
+        shortName: 'ST',
+        value: ''
+      }, {
+        name: 'localityName',
+        value: ''
+      }, {
+        name: 'organizationName',
+        value: 'fabric-remote-monitor'
+      }, {
+        shortName: 'OU',
+        value: 'fabric-remote-monitor'
+      }]
+
+    cert.setSubject(attrs)
+    cert.setIssuer(attrs)
+    cert.setExtensions([{
+        name: 'basicConstraints',
+        cA: true
+      }, {
+        name: 'keyUsage',
+        keyCertSign: true,
+        digitalSignature: true,
+        nonRepudiation: true,
+        keyEncipherment: true,
+        dataEncipherment: true
+      }, {
+        name: 'extKeyUsage',
+        serverAuth: true,
+        clientAuth: true,
+        codeSigning: true,
+        emailProtection: true,
+        timeStamping: true
+      }, {
+        name: 'nsCertType',
+        client: true,
+        server: true,
+        email: true,
+        objsign: true,
+        sslCA: true,
+        emailCA: true,
+        objCA: true
+      }, {
+        name: 'subjectKeyIdentifier'
+      }]);
+
+    cert.sign(keys.privateKey)
+
+    fs.writeFileSync(keyPath, pki.privateKeyToPem(keys.privateKey))
+    fs.writeFileSync(certPath, pki.certificateToPem(cert))
+
     console.log("Done generating SSL certificate")
 }
 
@@ -28,6 +91,7 @@ const app = express()
 const httpsServer = https.createServer({
     key: fs.readFileSync(keyPath),
     cert: fs.readFileSync(certPath),
+    
 }, app)
 
 const port = 8000
@@ -35,7 +99,7 @@ const port = 8000
 app.use(express.static("./build"))
 
 // fuser -k 8000/tcp
-app.listen(port)
+httpsServer.listen(port)
 console.log(`Web server running on port ${port}`)
 
 const minecraftInterface = new MinecraftInterface(8080)
