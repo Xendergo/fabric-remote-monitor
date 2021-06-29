8409b382a8d6d4f10cd980ca2ad0d42dad91fa9e
Xendergo <h.rovnyak@gmail.com>
2021-06-29 13:34:06 -0400

    Input field type

--- server/networking/sendableTypes.ts
+++ server/networking/sendableTypes.ts
@@ -176,3 +176,101 @@ interface Style {
     strikethrough: boolean
     obfuscated: boolean
 }
+
+type AllowedInputFieldTypes = "string" | "bool" | "number"
+
+type InputFieldsTypes<T> = {
+    +readonly [Property in keyof T]: AllowedInputFieldTypes
+}
+
+type InputFieldsClasses<T> = {
+    +readonly [Property in keyof T]: AllowedInputFieldTypes
+}
+
+type InputFieldClass<T> = {
+    channel(): string
+    new (value: T): { value: T }
+}
+
+function generateClass(
+    channel: string,
+    key: string,
+    type: AllowedInputFieldTypes
+) {
+    let ret: InputFieldClass<any>
+    if (type == "string") {
+        ret = class extends Sendable {
+            constructor(value: string | null) {
+                super()
+                this.value = value
+            }
+
+            static channel() {
+                return this.prototype.channel as string
+            }
+
+            value: string | null
+        }
+    } else if (type == "bool") {
+        ret = class extends Sendable {
+            constructor(value: boolean) {
+                super()
+                this.value = value
+            }
+
+            static channel() {
+                return this.prototype.channel as string
+            }
+
+            value: boolean
+        }
+    } /*if (type == "number")*/ else {
+        ret = class extends Sendable {
+            constructor(value: number | null) {
+                super()
+                this.value = value
+            }
+
+            static channel() {
+                return this.prototype.channel as string
+            }
+
+            value: number | null
+        }
+    }
+
+    ret.prototype.channel = `${channel}.${key}`
+
+    return ret
+}
+
+export class InputFields<T> {
+    constructor(channel: string, fields: InputFieldsTypes<T>) {
+        this.fields = Object.entries(fields)
+            .map(v => {
+                const [key, value] = v as [string, AllowedInputFieldTypes]
+
+                return [key, generateClass(channel, key, value)] as [
+                    string,
+                    InputFieldClass<AllowedInputFieldTypes>
+                ]
+            })
+            .reduce<{ [key: string]: InputFieldClass<AllowedInputFieldTypes> }>(
+                (a, v) => {
+                    a[v[0]] = v[1]
+                    return a
+                },
+                {}
+            ) as unknown as InputFieldsClasses<T>
+    }
+
+    fields: InputFieldsClasses<T>
+}
+
+interface DiscordInput {
+    token: string | null
+}
+
+export const discordInput = new InputFields<DiscordInput>("DiscordInput", {
+    token: "string",
+})
