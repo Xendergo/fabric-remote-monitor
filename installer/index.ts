9269d216ec40c9fda7aff555a3a09a8f42a3402f
Matthew Chen <Matgenius04@gmail.com>
2021-07-04 14:12:48 -0500

    Starting setup installer svelte project

--- installer/index.ts
+++ installer/index.ts
@@ -3,3 +3,22 @@
 
 // NO SSL yet maybe because that's evil
 import express from "express"
+// import kill from "kill-port";
+import { installFabric } from "./fabric-install"
+
+
+const app = express();
+
+const port = 8080;
+
+app.use(express.static('build'));
+
+// app.get('/', (req, res) => {
+//   res.send('<html>bruh</html>');
+//   res.end();
+// })
+
+
+console.log(`Server listening on port ${port}`);
+app.listen(port);
+// installFabric();
