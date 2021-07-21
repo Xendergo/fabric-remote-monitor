2b25362183136483fd16929eed4bab4d2dd9cc13
Xendergo <h.rovnyak@gmail.com>
2021-07-21 12:06:23 -0400

    Add documentation for the type checking system

--- sendableTypes/sendableTypesHelpers.ts
+++ sendableTypes/sendableTypesHelpers.ts
@@ -179,6 +179,12 @@ type EachType<T extends object> = {
  * A set of common strategies that you can input into {@link MakeSendable} so you don't have to constantly retype the same long functions
  */
 class Strategies {
+    /**
+     * Returns a strategy requiring all the strategies inputted to be true
+     * @param strats The strategies to check
+     * @returns A function representing all the strategies inputted being true
+     * @typeParam `T` - The type you're checking for
+     */
     all<T>(...strats: ((data: any) => boolean)[]) {
         return function (data: any): data is T {
             for (const strat of strats) {
@@ -191,6 +197,12 @@ class Strategies {
         }
     }
 
+    /**
+     * Returns a strategy requiring any the strategies inputted to be true
+     * @param strats The strategies to check
+     * @returns A function representing any the strategies inputted being true
+     * @typeParam `T` - The type you're checking for
+     */
     any<T>(...strats: ((data: any) => boolean)[]) {
         return function (data: any): data is T {
             for (const strat of strats) {
@@ -203,6 +215,12 @@ class Strategies {
         }
     }
 
+    /**
+     * Takes in an object type, and an object of strategies for each key in the object type, and returns a strategy for that object type
+     * @param strats The strategies for checking each object property
+     * @returns A function representing a strategy for checking the object type
+     * @typeParam `T` - The object type you're checking for
+     */
     each<T extends { [key: string]: any }>(strats: EachType<T>) {
         return function (data: any): data is T {
             for (const key in strats) {
@@ -223,6 +241,12 @@ class Strategies {
         }
     }
 
+    /**
+     * Takes in a tuple type, and a tuple of strategies for each item, and returns a strategy for checking the tuple type
+     * @param strats A tuple of strategies for each item in the tuple type
+     * @returns A strategy for type checking the tuple type
+     * @typeParam `T` - The tuple type you're checking for
+     */
     tupleEach<T extends Array<any>>(strats: EachType<T>) {
         return function (data: any): data is T {
             if (!Array.isArray(data) || data.length !== strats.length) {
@@ -239,6 +263,12 @@ class Strategies {
         }
     }
 
+    /**
+     * Generates a strategy for type checking an array by applying a strategy to each element in the array
+     * @param strat The strategy to apply to each element in the array
+     * @returns A strategy for type checking the whole array
+     * @typeParam `T` - The type you expect to be contained by the array
+     */
     Array<T>(strat: (data: any) => data is T) {
         return function (data: any): data is Array<T> {
             if (!Array.isArray(data)) {
@@ -255,50 +285,88 @@ class Strategies {
         }
     }
 
+    /**
+     * Returns a strategy that ensures that the value being checked is triple equal to the value passed into this function
+     * @param value The value to check against
+     * @returns A strategy for type checking T
+     * @typeParam `T` - The type you're checking for
+     */
     value<T>(value: T) {
         return function (data: any): data is T {
             return data === value
         }
     }
 
+    /**
+     * Returns a strategy that ensures that a value doesn't exist, useful if you know the value is contained in the class prototype
+     * @returns A strategy ensuring type T doesn't exist
+     * @typeParam `T` - The type you're checking for
+     */
     isPrototype<T>() {
         return function (data: any): data is T {
             return data === undefined
         }
     }
 
+    /**
+     * A strategy checking if the value is a number
+     */
     number(data: any): data is number {
         return typeof data === "number"
     }
 
+    /**
+     * A strategy checking if the value is a string
+     */
     string(data: any): data is string {
         return typeof data === "string"
     }
 
+    /**
+     * A strategy checking if the value is a boolean
+     */
     boolean(data: any): data is boolean {
         return typeof data === "boolean"
     }
 
+    /**
+     * A strategy checking if the value is a bigint
+     */
     bigint(data: any): data is bigint {
         return typeof data === "bigint"
     }
 
+    /**
+     * A strategy checking if the value is a symbol
+     */
     symbol(data: any): data is symbol {
         return typeof data === "symbol"
     }
 
+    /**
+     * A strategy checking if the value is a function
+     */
     function(data: any): data is Function {
         return typeof data === "function"
     }
 
+    /**
+     * A strategy checking if the value is an object
+     */
     object(data: any): data is object {
         return typeof data === "object"
     }
 
+    /**
+     * A strategy checking if the value is null
+     */
     null(data: any): data is null {
         return data === null
     }
 
+    /**
+     * A strategy checking if the value is undefined
+     */
     undefined(data: any): data is undefined {
         return data === undefined
     }
@@ -324,6 +392,7 @@ const sendableClasses: Map<
 /**
  * A decorator to make a class sendable via websockets
  * @param channel The channel this class should be sent through
+ * @param strategies An object of strategies for type checking the values sent representing this class, in case someone sends invalid information to the server
  */
 export function MakeSendable<T extends Sendable>(
     channel: string,
